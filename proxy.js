b8f14c76c307611e9c691a97dc193d4191e76ff4
BScrk <luca.benevolo@gmail.com>
2017-10-30 11:42:15 +0100

    Bugfix while pool connection

--- proxy.js
+++ proxy.js
@@ -1,17 +1,19 @@
 
 var remotePool = require('./lib/pool_connector.js');
 var logger = require('./lib/stratum_logger.js');
+var info = require('./package.json');
+
 var poolProxy = null;
 
 
 
 var startStratum = function() {
-    logger.log("ZEC STRATUM PROXY STARTING...");        
+    logger.log("ZEC STRATUM PROXY "+ info.version +" STARTING...");        
     poolProxy = new remotePool.PoolConnector(restartStratum);
 }
 
 var restartStratum = function() {
-    logger.warn("ZEC STRATUM PROXY RESTARTING...");
+    logger.warn("ZEC STRATUM PROXY RESETING...");
     poolProxy.destroy();
     delete poolProxy;
     startStratum();
